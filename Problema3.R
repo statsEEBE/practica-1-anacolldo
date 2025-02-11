19aba450fa57f9ace988946d665e01e7f8d0b680
ana.collado.morata <ana.collado.morata@auleseebe.upc.edu>
2025-02-11 09:56:14 +0100

    solucio problema 3

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,9 @@
 #Codigo para problema 3
+x<- c(-8,5,2,-8,9,5,2,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5)
+sum(x) #suma de todos los valores
+
+y<- x[-c(21,2,26)] #vector creado eliminando las posicione 21,2,26 
+sum(exp(x))-sum(exp(y)) #suma exponenciales elemento a elemento y resta
+
+y[19] #dada de la posicio 19 en el vector y
+y[24] #dada de la posicio 24 en el vector y
